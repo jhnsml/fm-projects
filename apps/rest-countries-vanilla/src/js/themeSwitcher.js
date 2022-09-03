dc5525d7fb85fe350871c2760f2cc85cccd02811
John Samuel <jhnsml.31@gmail.com>
2022-09-04 04:22:09 +0530

    feat: add localStorage support for theme switcher

--- apps/rest-countries-vanilla/src/js/themeSwitcher.js
+++ apps/rest-countries-vanilla/src/js/themeSwitcher.js
@@ -4,26 +4,52 @@ const ICON_CLASS = {
 	dark: "fa-sun",
 	light: "fa-moon",
 };
+
+const THEMES = {
+	LIGHT: "light",
+	DARK: "dark",
+};
+
+const THEME_KEY = "theme";
 export const initializeTheme = () => {
-	const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+	const cachedTheme = localStorage.getItem(THEME_KEY);
 
-	const themeText = isDarkTheme ? "Dark" : "Light";
-	document.documentElement.setAttribute("data-theme", themeText.toLowerCase());
+	if (cachedTheme !== null) {
+		document.documentElement.setAttribute("data-theme", cachedTheme);
+		elements.currentTheme.textContent = `${cachedTheme} Mode`;
+		elements.themeIcon.classList.add(`${ICON_CLASS[cachedTheme]}`);
+	} else {
+		const isDarkTheme = window.matchMedia(
+			`(prefers-color-scheme:${THEMES.DARK})`
+		).matches;
+		const theme = isDarkTheme ? THEMES.DARK : THEMES.LIGHT;
 
-	elements.currentTheme.textContent = `${themeText} Mode`;
-	elements.themeIcon.classList.add(`${ICON_CLASS[themeText.toLowerCase()]}`);
+		localStorage.setItem(THEME_KEY, theme);
+		document.documentElement.setAttribute("data-theme", theme);
+		elements.currentTheme.textContent = `${theme} Mode`;
+		elements.themeIcon.classList.add(`${ICON_CLASS[theme]}`);
+	}
 };
 
 export const switchTheme = (currentTheme) => {
 	const prevIconClass = ICON_CLASS[currentTheme];
 	document.documentElement.removeAttribute("data-theme");
-	if (currentTheme !== "dark") {
-		document.documentElement.setAttribute("data-theme", "dark");
-		elements.currentTheme.textContent = `Dark Mode`;
-		elements.themeIcon.classList.replace(prevIconClass, ICON_CLASS["dark"]);
+
+	if (currentTheme !== THEMES.DARK) {
+		document.documentElement.setAttribute("data-theme", THEMES.DARK);
+		elements.currentTheme.textContent = `${THEMES.DARK} Mode`;
+		elements.themeIcon.classList.replace(
+			prevIconClass,
+			ICON_CLASS[THEMES.DARK]
+		);
+		localStorage.setItem(THEME_KEY, THEMES.DARK);
 	} else {
-		document.documentElement.setAttribute("data-theme", "light");
-		elements.currentTheme.textContent = `Light Mode`;
-		elements.themeIcon.classList.replace(prevIconClass, ICON_CLASS["light"]);
+		document.documentElement.setAttribute("data-theme", THEMES.LIGHT);
+		elements.currentTheme.textContent = `${THEMES.LIGHT} Mode`;
+		elements.themeIcon.classList.replace(
+			prevIconClass,
+			ICON_CLASS[THEMES.LIGHT]
+		);
+		localStorage.setItem(THEME_KEY, THEMES.LIGHT);
 	}
 };
